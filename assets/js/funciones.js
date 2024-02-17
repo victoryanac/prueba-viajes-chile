8fe1bf88c5ecd44c6fd108fd346feed25a5bfce5
Victor Yanac <victorsyt.94@gmail.com>
2024-02-16 23:50:22 -0300

    personalizacion css seccion (contacto y footer)

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,12 +1,2 @@
 
-/* navbar */
-document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 50) {
-            document.getElementById('mainNav').classList.add('navbar-scrolled');
-        } else {
-            document.getElementById('mainNav').classList.remove('navbar-scrolled');
-        }
-    });
-});
 
