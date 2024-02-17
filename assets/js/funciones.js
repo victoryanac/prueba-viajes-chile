8a6f6f72f24e12d083dec94a25b88cfc9bc9b144
Victor Yanac <victorsyt.94@gmail.com>
2024-02-16 23:52:21 -0300

    aplicacion de funciones con JS

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,2 +1,20 @@
+/* navbar */
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            document.getElementById('mainNav').classList.add('navbar-scrolled');
+        } else {
+            document.getElementById('mainNav').classList.remove('navbar-scrolled');
+        }
+    });
+});
+/* SECCION CONTACTO */
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+$("#enviarCorreo").click(
+    function () {
+        alert("Tu mensaje fue enviado correctamente...");
+    }
+)
 
